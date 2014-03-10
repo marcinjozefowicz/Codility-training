7a2db87467b045aec9ccd6b08f6889e4423583f3
marcinjozefowicz <marcin_jozefowicz@o2.pl>
2014-03-10 22:16:04 +0100

    Proper solution based on http://codesays.com/2014/solution-to-min-avg-two-slice-by-codility/

--- IdeaProjects/untitled/src/MinAvgTwoSlice.java
+++ IdeaProjects/untitled/src/MinAvgTwoSlice.java
@@ -6,5 +6,36 @@
  * To change this template use File | Settings | File Templates.
  */
 public class MinAvgTwoSlice {
-    public static solution()
+    public static int solution(int[] A) {
+        double minimal = 10000;
+        int startingPosition   = 0;
+
+        for(int i=0;i<A.length-2;i++){
+            if((A[i]+A[i+1])/2.0<minimal){
+                minimal = (A[i]+A[i+1])/2.0;
+                startingPosition = i;
+            }
+        }
+
+        for(int i=0;i<A.length-2;i++){
+            if((A[i]+A[i+1]+A[i+2])/3.0<minimal){
+                minimal = (A[i]+A[i+1]+A[i+2])/3.0;
+                startingPosition = i;
+            }
+        }
+
+        // comparing the last two elements
+
+        if((A[A.length-2]+A[A.length-1])/2.0<minimal){
+            minimal = (A[A.length-2]+A[A.length-1])/2.0;
+            startingPosition = A.length-2;
+        }
+
+        return startingPosition;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MinAvgTwoSlice.solution(new int[]{4, 2, 2, 5, 1, 5, 8}));
+    }
 }
